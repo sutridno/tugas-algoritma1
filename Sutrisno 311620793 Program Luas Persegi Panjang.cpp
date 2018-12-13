dbf1579d5d6aad0ba0959f6ec327e3b38086fed5
sutrisno trisno <39906761+sutridno@users.noreply.github.com>
2018-12-13 20:45:45 +0700

    Tugas Algoritma
    Tugas 1

---
+++ Sutrisno 311620793 Program Luas Persegi Panjang.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main () {
+	int p, l, luas;
+	
+	cout<<"masukan panjang:";
+	cin>>p;
+	cout<<"masukan lebar:";
+	cin>>l;
+	luas=p*l;
+	cout<<"luas persegi panjang adalah"<<luas;
+	return 0;
+}
